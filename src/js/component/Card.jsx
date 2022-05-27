bf55af51adbcadecce274246b688d1ca4adbef8e
majg1987 <majg1987@hotmail.com>
2022-05-27 09:40:57 +0000

    Props añadidas

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+	const { src, title } = props;
 	return (
 		<div className="card text-center mb-5">
-			<img
-				src="https://picsum.photos/200"
-				className="card-img-top"
-				alt="img"
-			/>
+			<img src={src} className="card-img-top" alt="img" />
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
+				<h5 className="card-title">{title}</h5>
 				<p className="card-text pb-5 border-bottom">
 					Some quick example text to build on the card title and make
 					up the bulk of the card's content.
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,16 +13,28 @@ const Home = () => {
 				<Jumbotron />
 				<div className="row d-flex justify-content-center">
 					<div className="col-md-6 col-lg-3">
-						<Card />
+						<Card
+							src="https://picsum.photos/id/1011/5472/3648"
+							title="Navegando por el Lago"
+						/>
 					</div>
 					<div className="col-md-6 col-lg-3">
-						<Card />
+						<Card
+							src="https://picsum.photos/id/110/5616/3744"
+							title="Atardecer"
+						/>
 					</div>
 					<div className="col-md-6 col-lg-3">
-						<Card />
+						<Card
+							src="https://picsum.photos/id/28/4928/3264"
+							title="Naturaleza"
+						/>
 					</div>
 					<div className="col-md-6 col-lg-3">
-						<Card />
+						<Card
+							src="https://picsum.photos/id/374/3888/2592"
+							title="Playas Virgenes"
+						/>
 					</div>
 				</div>
 			</div>
